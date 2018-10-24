0f3a3fbc62020326fcad5cfb17dbb705d319ce4e
Ana Mafla <anamafla@hotmail.com>
2018-10-23 20:02:08 -0500

    Firebase and Firestore

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var config = {
+  apiKey: procces.env.REACT_APP_API_KEY,
+  authDomain: "anacoding2018.firebaseapp.com",
+  databaseURL: "https://anacoding2018.firebaseio.com",
+  projectId: "anacoding2018",
+  storageBucket: "anacoding2018.appspot.com",
+  messagingSenderId: "458360476947"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
